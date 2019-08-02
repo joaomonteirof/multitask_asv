c9c8932bcb2a3b8229b01c2b31726882d892dafd
joaomonteirof <joaomonteirof@gmail.com>
2019-08-02 09:02:52 -0400

    fix on lr scheduling

--- train_loop.py
+++ train_loop.py
@@ -52,9 +52,9 @@ class TrainLoop(object):
 
 		if self.valid_loader is not None:
 			self.history['valid_loss'] = []
-			self.after_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, factor=0.5, patience=patience*len(train_loader.dataset)//(train_loader.batch_size*5), verbose=True if self.verbose>0 else False, threshold=1e-4, min_lr=1e-7)
+			self.after_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, factor=0.5, patience=patience*len(train_loader.dataset)//(train_loader.batch_size), verbose=True if self.verbose>0 else False, threshold=1e-4, min_lr=1e-7)
 		else:
-			self.after_scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[3, 10, 50, 100]*(len(train_loader.dataset)//(train_loader.batch_size*5)), gamma=0.1)
+			self.after_scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[3, 10, 50, 100]*(len(train_loader.dataset)//(train_loader.batch_size)), gamma=0.1)
 
 		if self.softmax:
 			self.history['softmax_batch']=[]
