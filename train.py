83ebaf9e4eb25772cc0d4107d0be81621eedb4e3
joaomonteirof <joaomonteirof@gmail.com>
2019-08-08 21:24:45 -0400

    fix on logging

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ if args.cuda:
 	torch.cuda.manual_seed(args.seed)
 
 if args.logdir:
-	writer = SummaryWriter(log_dir=args.logdir, comment=args.model)
+	writer = SummaryWriter(log_dir=args.logdir, comment=args.model, purge_step=True)
 else:
 	writer = None
 
@@ -150,6 +150,6 @@ if args.verbose >0:
 	print(' ')
 
 if writer:
-	writer.add_graph(model=model, input_to_model=torch.rand(2, 3 if args.delta else 1, args.ncoef, 200).to(device), operator_export_type='RAW')
+	writer.add_graph(model=model, input_to_model=torch.rand(2, 3 if args.delta else 1, args.ncoef, 200).to(device))
 
 trainer.train(n_epochs=args.epochs, save_every=args.save_every)
