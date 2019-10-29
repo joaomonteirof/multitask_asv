d8373f9dd87bde22893de7efc5daebb9702d1e10
joaomonteirof <joaomonteirof@gmail.com>
2019-10-29 12:31:53 -0400

    smaller transformer

--- model.py
+++ model.py
@@ -1084,11 +1084,11 @@ class transformer_enc(nn.Module):
 		self.pre_encoder = nn.Sequential( nn.Conv1d(3*ncoef if delta else ncoef, 512, 7),
 			nn.BatchNorm1d(512),
 			nn.ReLU(inplace=True),
-			nn.Conv1d(512, 512, 7),
+			nn.Conv1d(512, 512, 5),
 			nn.BatchNorm1d(512),
 			nn.ReLU(inplace=True) )
 
-		self.transformer_encoder = nn.TransformerEncoder(nn.TransformerEncoderLayer(d_model=512, nhead=8, dim_feedforward=768, dropout=0.1), num_layers=6, norm=nn.LayerNorm(512) )
+		self.transformer_encoder = nn.TransformerEncoder(nn.TransformerEncoderLayer(d_model=512, nhead=8, dim_feedforward=768, dropout=0.1), num_layers=5, norm=nn.LayerNorm(512) )
 
 		self.pooling = StatisticalPooling()
 
