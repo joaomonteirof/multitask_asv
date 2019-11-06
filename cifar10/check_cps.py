bc57f2ffe129512b30593c897673a75eb2e3f370
joaomonteirof <joaomonteirof@gmail.com>
2019-11-06 16:33:16 -0500

    adding script to search cps - cifar

---
+++ cifar10/check_cps.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+import os
+import argparse
+import torch
+import glob
+from models import vgg, resnet, densenet
+
+# Training settings
+parser = argparse.ArgumentParser(description='Test new architectures')
+parser.add_argument('--cp-path', type=str, default=None, metavar='Path', help='Path for checkpoints')
+parser.add_argument('--model', choices=['vgg', 'resnet', 'densenet'], default='resnet')
+parser.add_argument('--softmax', choices=['softmax', 'am_softmax'], default='softmax', help='Softmax type')
+args = parser.parse_args()
+
+if args.model == 'vgg':
+	model = vgg.VGG('VGG16', sm_type=args.softmax)
+elif args.model == 'resnet':
+	model = resnet.ResNet18(sm_type=args.softmax)
+elif args.model == 'densenet':
+	model = densenet.densenet_cifar(sm_type=args.softmax)
+
+cp_list = glob.glob(args.cp_path+'*.pt')
+
+assert len(cp_list)>0, 'No cp found in the given path!'
+
+for cp in cp_list:
+
+	ckpt = torch.load(cp, map_location = lambda storage, loc: storage)
+	try:
+		model.load_state_dict(ckpt['model_state'], strict=True)
+		print('lol', cp.split('/')[-1])
+	except:
+		pass
