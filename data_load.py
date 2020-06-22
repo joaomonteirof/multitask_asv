0e073277be3b764c59d14d400862c43554c66902
joaomonteirof <joaomonteirof@gmail.com>
2020-06-22 14:28:21 -0400

    included distortions on the spectra for augmentation

--- data_load.py
+++ data_load.py
@@ -81,13 +81,13 @@ class Loader(Dataset):
 
 		if not self.open_file: self.open_file = h5py.File(self.hdf5_name, 'r')
 
-		utt_1_data = torch.from_numpy( self.prep_utterance( self.open_file[spk][utt_1] ) )
-		utt_2_data = torch.from_numpy( self.prep_utterance( self.open_file[spk][utt_2] ) )
-		utt_3_data = torch.from_numpy( self.prep_utterance( self.open_file[spk][utt_3] ) )
-		utt_4_data = torch.from_numpy( self.prep_utterance( self.open_file[spk][utt_4] ) )
-		utt_5_data = torch.from_numpy( self.prep_utterance( self.open_file[spk][utt_5] ) )
+		utt_1_data = self.prep_utterance( self.open_file[spk][utt_1] )
+		utt_2_data = self.prep_utterance( self.open_file[spk][utt_2] )
+		utt_3_data = self.prep_utterance( self.open_file[spk][utt_3] )
+		utt_4_data = self.prep_utterance( self.open_file[spk][utt_4] )
+		utt_5_data = self.prep_utterance( self.open_file[spk][utt_5] )
 
-		return utt_1_data.contiguous(), utt_2_data.contiguous(), utt_3_data.contiguous(), utt_4_data.contiguous(), utt_5_data.contiguous(), y
+		return utt_1_data, utt_2_data, utt_3_data, utt_4_data, utt_5_data, y
 
 	def __len__(self):
 		return len(self.utt_list)
@@ -101,6 +101,8 @@ class Loader(Dataset):
 			mul = int(np.ceil(self.max_nb_frames/data.shape[-1]))
 			data_ = np.tile(data, (1, 1, mul))
 			data_ = data_[:, :, :self.max_nb_frames]
+
+		data_ = torch.from_numpy(data_).float().contiguous()
 
 		data_ = augment_spec(data_)
 
@@ -161,16 +163,15 @@ class Loader_valid(Dataset):
 		if not self.open_file: self.open_file = h5py.File(self.hdf5_name, 'r')
 
 		utt_data = self.prep_utterance( self.open_file[spk][utt] )
-		utt_data = torch.from_numpy( utt_data )
 
 		utt_1, utt_2, utt_3, utt_4 = np.random.choice(self.spk2utt[spk], 4)
 
-		utt_1_data = torch.from_numpy( self.prep_utterance( self.open_file[spk][utt_1] ) )
-		utt_2_data = torch.from_numpy( self.prep_utterance( self.open_file[spk][utt_2] ) )
-		utt_3_data = torch.from_numpy( self.prep_utterance( self.open_file[spk][utt_3] ) )
-		utt_4_data = torch.from_numpy( self.prep_utterance( self.open_file[spk][utt_4] ) )
+		utt_1_data = self.prep_utterance( self.open_file[spk][utt_1] )
+		utt_2_data = self.prep_utterance( self.open_file[spk][utt_2] )
+		utt_3_data = self.prep_utterance( self.open_file[spk][utt_3] )
+		utt_4_data = self.prep_utterance( self.open_file[spk][utt_4] )
 
-		return utt_data.contiguous(), utt_1_data.contiguous(), utt_2_data.contiguous(), utt_3_data.contiguous(), utt_4_data.contiguous(), self.utt2label[utt]
+		return utt_data, utt_1_data, utt_2_data, utt_3_data, utt_4_data, self.utt2label[utt]
 
 	def __len__(self):
 		return len(self.utt_list)
@@ -187,6 +188,8 @@ class Loader_valid(Dataset):
 
 		if self.delta:
 			data_ = np.concatenate([data_, delta_(data_,width=3,order=1), delta_(data_,width=3,order=2)], axis=0)
+
+		data_ = torch.from_numpy(data_).float().contiguous()
 
 		return data_
 
@@ -210,97 +213,6 @@ class Loader_valid(Dataset):
 				self.utt_list.append(utt)
 
 		open_file.close()
-
-class Loader_test(Dataset):
-
-	def __init__(self, hdf5_name):
-		super(Loader_test, self).__init__()
-		self.hdf5_name = hdf5_name
-
-		self.create_lists()
-
-		self.open_file = None
-
-		self.update_lists()
-
-	def __getitem__(self, index):
-
-		utt_1, utt_2, utt_3, utt_4, utt_5, spk, y= self.utt_list[index]
-
-		assert utt_1 in self.spk2utt[spk] and utt_2 in self.spk2utt[spk] and utt_3 in self.spk2utt[spk] and utt_4 in self.spk2utt[spk] and utt_5 in self.spk2utt[spk]
-
-		utt_list_ = [utt_1, utt_2, utt_3, utt_4, utt_5]
-
-		assert len(utt_list_) == len(set(utt_list_))
-
-		return utt_1.contiguous(), utt_2.contiguous(), utt_3.contiguous(), utt_4.contiguous(), utt_5.contiguous(), spk, y
-
-	def __len__(self):
-		return len(self.utt_list)
-
-	def prep_utterance(self, data):
-
-		if data.shape[-1]>self.max_nb_frames:
-			ridx = np.random.randint(0, data.shape[-1]-self.max_nb_frames)
-			data_ = data[:, :, ridx:(ridx+self.max_nb_frames)]
-		else:
-			mul = int(np.ceil(self.max_nb_frames/data.shape[-1]))
-			data_ = np.tile(data, (1, 1, mul))
-			data_ = data_[:, :, :self.max_nb_frames]
-
-		if self.delta:
-			data_ = np.concatenate([data_, delta_(data_,width=3,order=1), delta_(data_,width=3,order=2)], axis=0)
-
-		return data_
-
-	def create_lists(self):
-
-		open_file = h5py.File(self.hdf5_name, 'r')
-
-		self.spk2label = {}
-		self.spk2utt = {}
-		self.utt_list = []
-
-		for i, spk in enumerate(open_file):
-			spk_utt_list = list(open_file[spk])
-			self.spk2utt[spk] = spk_utt_list
-			self.spk2label[spk] = torch.LongTensor([i])
-
-		open_file.close()
-
-		self.n_speakers = len(self.spk2utt)
-
-	def update_lists(self):
-
-		self.utt_list = []
-
-		print('\nNew List!!\n')
-
-		utt_count = 0
-		included_utt_count = 0
-
-		for i, spk in enumerate(self.spk2utt):
-			spk_utt_list = np.random.permutation(list(self.spk2utt[spk]))
-
-			utt_count += len(spk_utt_list)
-
-			idxs = strided_app(np.arange(len(spk_utt_list)),5,5)
-
-			for idxs_list in idxs:
-
-				if len(idxs_list)==5:
-					self.utt_list.append([spk_utt_list[utt_idx] for utt_idx in idxs_list])
-					included_utt_count+=len(self.utt_list[-1])
-					if len(self.utt_list)>1:
-						assert len(set(self.utt_list[-1]) & set(self.utt_list[-2]))==0
-					self.utt_list[-1].append(spk)
-					self.utt_list[-1].append(self.spk2label[spk])
-
-		print('Total utts and included utts: {}, {}'.format(utt_count,included_utt_count))
-
-		tot_list = [item for sublist in self.utt_list for item in sublist[:-2]]
-
-		print(5*len(self.utt_list), len(tot_list))
 
 if __name__=='__main__':
 
