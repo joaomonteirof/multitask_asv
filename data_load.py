fdd45af2e91efe2fa18d21ed1ae6b84249a3cc34
joaomonteirof <joaomonteirof@gmail.com>
2020-08-21 14:08:15 -0400

    more flip augmentation

--- data_load.py
+++ data_load.py
@@ -16,7 +16,7 @@ def augment_spec(example):
 			example = freq_mask(example, F=300, dim=2)
 		if random.random()>0.5:
 			example += torch.randn_like(example)*random.choice([1e-1, 1e-2])
-		if random.random()>0.8:
+		if random.random()>0.5:
 			example = torch.flip(example, dims=[-1])
 
 	return example
